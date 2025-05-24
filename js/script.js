60a590896109adc99517c3d3f216caf8c3bc5f01
elix-stack <muhammad.ali.yusufgi@gmail.com>
2025-05-24 17:48:43 +0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+//ketika hamburger diklik kemudian muncul sidebar, klik lagi hilang
+document.querySelector('#hamburger-menu').onclick = () => {
+  navbarNav.classList.toggle('active');
+};
+
+//klik diluar hamburger = sidebar hilang
+const hamburger = document.querySelector('#hamburger-menu');
+document.addEventListener('click', function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
