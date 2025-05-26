49e8544585f6675c4de0e9e12aca8297c4fc75b7
elix-stack <muhammad.ali.yusufgi@gmail.com>
2025-05-26 22:47:22 +0700

    update search button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
-//toggle class active
+//toggle class active hamburger menu
 const navbarNav = document.querySelector('.navbar-nav');
 //ketika hamburger diklik kemudian muncul sidebar, klik lagi hilang
-document.querySelector('#hamburger-menu').onclick = () => {
+document.querySelector('#hamburger-menu').onclick = (e) => {
   navbarNav.classList.toggle('active');
+  e.preventDefault();
 };
 
-//klik diluar hamburger = sidebar hilang
+//toggle class active search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+//ketika hamburger diklik kemudian muncul sidebar, klik lagi hilang
+document.querySelector('#search-button').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+
+//klik diluar element
 const hamburger = document.querySelector('#hamburger-menu');
+const searchButton = document.querySelector('#search-button');
+
 document.addEventListener('click', function (e) {
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+  if (!searchButton.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
   }
 });
